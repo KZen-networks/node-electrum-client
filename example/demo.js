57ea1aba8c7074bf20c0b5326f4cf3ecbf69f433
Roman Zeyde <me@romanzey.de>
2018-08-05 11:52:11 +0300

    Added demo for 'blockhain.address.getbalance' and verbose 'blockchain.transaction.get'

--- example/demo.js
+++ example/demo.js
@@ -8,8 +8,12 @@ const main = async () => {
         console.log(ver)
         const unspent = await ecl.blockchainAddress_listunspent("1LcgGw9r2FJMGPCLGtWeQRNcAwHfXjzNTm")
         console.log(unspent)
-        const tx1 = await ecl.blockchainTransaction_get("949bd856593a9dce310db3063898cde8fc8de548e20b9890294a16b3f384fef4", false)
-        console.log(tx1)
+        const balance = await ecl.blockchainAddress_getBalance("1LcgGw9r2FJMGPCLGtWeQRNcAwHfXjzNTm")
+        console.log(balance)
+        const tx = await ecl.blockchainTransaction_get("949bd856593a9dce310db3063898cde8fc8de548e20b9890294a16b3f384fef4", false)
+        console.log(tx)
+        const vtx = await ecl.blockchainTransaction_get("949bd856593a9dce310db3063898cde8fc8de548e20b9890294a16b3f384fef4", true)
+        console.log(vtx)
     } catch(e){
         console.log(e)
     }
